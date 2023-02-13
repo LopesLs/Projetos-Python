a641ef49e779d796fe641fd65ce066c385650042
Carlos Linux <lopes.carlos.host@gmail.com>
2023-02-13 13:30:36 -0300

    feat: home ambient and fix icons

--- IMCalc/software.py
+++ IMCalc/software.py
@@ -19,19 +19,24 @@ class Software(customtkinter.CTk):
         self.loginWindow = customtkinter.CTkFrame(self)
         self.loginWindow.pack(pady=20, padx=60, fill="both", expand=True)
 
-        self.topText = customtkinter.CTkLabel(master=self.loginWindow, text="Sistema de login", font=self.titleFont)
+        self.topText = customtkinter.CTkLabel(master=self.loginWindow, text="Seja bem-vindo(a)!", font=self.titleFont)
         self.topText.pack(pady=12, padx=10)
 
         self.inputName = customtkinter.CTkEntry(master=self.loginWindow, placeholder_text="Insira seu nome")
+        self.inputName.configure(font=self.textFont, justify=customtkinter.CENTER)
         self.inputName.pack(pady=12, padx=10)
 
-        self.buttonNext = customtkinter.CTkButton(master=self.loginWindow, corner_radius=15, text="login", command=self.imcFrame)
+        self.buttonNext = customtkinter.CTkButton(master=self.loginWindow, corner_radius=15, text="Entrar", command=self.imcFrame, font=self.textFont)
         self.buttonNext.pack(pady=12, padx=10)
 
-        self.check = customtkinter.CTkCheckBox(master=self.loginWindow, text="Manter-se logado")
+        self.check = customtkinter.CTkCheckBox(master=self.loginWindow, text="Manter-se logado", font=self.textFont)
         self.check.pack(pady=12, padx=10)
 
     def imcFrame(self):
+        
+        print(self.inputName.get())
+        print(self.check.get())
+        
         self.loginWindow.pack_forget()
         
         self.mainMenu = customtkinter.CTkFrame(self)
@@ -41,9 +46,16 @@ class Software(customtkinter.CTk):
         self.topText.pack(pady=12, padx=10)
 
     def style(self):
-        self.titleFont = customtkinter.CTkFont(family="Nunito",size=24, weight="bold")
+        self.titleFont = customtkinter.CTkFont(family="Miriam",size=24, weight="bold")
+        self.textFont = customtkinter.CTkFont(family="Miriam Fixed", size=13)
 
 if __name__ == "__main__":
     app = Software()
-    app.iconbitmap('assets/icon/img0.ico')
+    
+    # If the computer don't support to icons, then ingore it.
+    try:
+        app.iconbitmap("assets/icon/img0.ico")
+    except:
+        pass
+    
     app.mainloop()
